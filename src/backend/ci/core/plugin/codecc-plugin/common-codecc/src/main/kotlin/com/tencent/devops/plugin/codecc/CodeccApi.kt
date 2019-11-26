0a4a3e6f6d73273f5ef83f911d0430834083fe49
ddlin <Dxj15625122975>
2019-11-26 10:41:01 +0800

    add codecc v2 support

--- src/backend/ci/core/plugin/codecc-plugin/common-codecc/src/main/kotlin/com/tencent/devops/plugin/codecc/CodeccApi.kt
+++ src/backend/ci/core/plugin/codecc-plugin/common-codecc/src/main/kotlin/com/tencent/devops/plugin/codecc/CodeccApi.kt
@@ -161,7 +161,7 @@ open class CodeccApi constructor(
         taskExecution(body, "$deletePath/$taskId", headers, "DELETE")
     }
 
-    fun getRuleSets(projectId: String, userId: String, toolName: String): Result<Map<String, List<CodeccToolRuleSet>>> {
+    fun getRuleSets(projectId: String, userId: String, toolName: String): Result<Map<String, Any>> {
         val headers = mapOf(
             AUTH_HEADER_DEVOPS_USER_ID to userId,
             AUTH_HEADER_DEVOPS_PROJECT_ID to projectId
@@ -175,25 +175,7 @@ open class CodeccApi constructor(
             headers = headers,
             method = "POST"
         )
-        val mapResult = objectMapper.readValue<Map<String, Any>>(result)
-        val checkerSets = mapResult["checkerSets"] as Map<String, Any>
-        val ruleSetResult = mutableListOf<CodeccToolRuleSet>()
-        checkerSets.forEach { key, value ->
-            if (key == "toolName") return@forEach
-            val childrenMap = value as List<Map<String, Any>>
-            ruleSetResult.add(CodeccToolRuleSet(
-                id = key,
-                name = "",
-                children = childrenMap.map{
-                    CodeccToolRuleSet.CodeccToolRuleSetChild(
-                        it["checkerSetId"] as String,
-                        it["checkerSetName"] as String,
-                        it["codeLangs"] as List<String>
-                        )
-                }
-            ))
-        }
-        return Result(mapOf("records" to ruleSetResult))
+        return objectMapper.readValue(result)
     }
 
     private fun taskExecution(
@@ -324,16 +306,4 @@ open class CodeccApi constructor(
         val toolName: String,
         val checkerSetId: String
     )
-
-    data class CodeccToolRuleSet(
-        val id: String,
-        val name: String,
-        val children: List<CodeccToolRuleSetChild>
-    ) {
-        data class CodeccToolRuleSetChild(
-            val ruleSetId: String,
-            val ruleSetName: String,
-            val codeLangs: List<String>
-        )
-    }
 }
