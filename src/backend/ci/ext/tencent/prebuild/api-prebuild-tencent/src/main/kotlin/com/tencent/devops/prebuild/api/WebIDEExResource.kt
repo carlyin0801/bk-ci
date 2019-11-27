06d7dc2e1a513dd62230527fef1ea10a5ad01ec0
camelxiao <siukimfong@qq.com>
2019-11-27 10:24:46 +0800

    ktlint fixed

--- src/backend/ci/ext/tencent/prebuild/api-prebuild-tencent/src/main/kotlin/com/tencent/devops/prebuild/api/WebIDEExResource.kt
+++ src/backend/ci/ext/tencent/prebuild/api-prebuild-tencent/src/main/kotlin/com/tencent/devops/prebuild/api/WebIDEExResource.kt
@@ -6,7 +6,12 @@ import com.tencent.devops.common.api.pojo.Result
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import javax.ws.rs.*
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["WEBIDE"], description = "WebIDE external 资源")
@@ -18,16 +23,14 @@ interface WebIDEExResource {
     @Path("/heartBeat")
     @ApiOperation("ide心跳上报接口")
     fun heartBeat(
-            @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
-            @HeaderParam(AUTH_HEADER_USER_ID)
-            userId: String,
-            @QueryParam("ip")
-            @ApiParam(value = "IDE实例的ip地址", required = true)
-            ip: String,
-            @QueryParam("version")
-            @ApiParam(value = "IDE实例的版本", required = true)
-            version: String
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @QueryParam("ip")
+        @ApiParam(value = "IDE实例的ip地址", required = true)
+        ip: String,
+        @QueryParam("version")
+        @ApiParam(value = "IDE实例的版本", required = true)
+        version: String
     ): Result<Boolean>
 }
-
-
