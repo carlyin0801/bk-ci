277747429c8addb5740aec40dab381b781c2324b
Royalhuang <royalhuang@tencent.com>
2019-12-14 20:56:12 +0800

    --story=856743929 devops日志组件升级

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/atom/vm/DispatchBuildLessDockerShutdownTaskAtom.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/atom/vm/DispatchBuildLessDockerShutdownTaskAtom.kt
@@ -81,14 +81,14 @@ class DispatchBuildLessDockerShutdownTaskAtom @Autowired constructor(
             )
         )
         // 设置Job日志区间终点
-        LogUtils.addRangeEndLine(
-            rabbitTemplate = rabbitTemplate,
-            buildId = task.buildId,
-            rangeName = task.containerHashId ?: task.containerId,
-            tag = task.containerHashId ?: "",
-            jobId = task.containerHashId,
-            executeCount = task.executeCount ?: 1
-        )
+//        LogUtils.addRangeEndLine(
+//            rabbitTemplate = rabbitTemplate,
+//            buildId = task.buildId,
+//            rangeName = task.containerHashId ?: task.containerId,
+//            tag = task.containerHashId ?: "",
+//            jobId = task.containerHashId,
+//            executeCount = task.executeCount ?: 1
+//        )
         // 同步Job执行状态
 //        LogUtils.stopLog(rabbitTemplate, buildId, task.containerHashId ?: "", task.containerHashId ?: "", task.executeCount)
 
