ea8dbb5466d0c78b3fa85eea8f01853ea4481f64
cao150260 <fitzcao@tencent.com>
2019-12-18 11:28:54 +0800

    --story=856453975 【OpenAPI】流水线可查看/执行权限API

--- src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/v2/ApigwProjectResourceV2.kt
+++ src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/v2/ApigwProjectResourceV2.kt
@@ -32,6 +32,7 @@ import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.project.api.pojo.PipelinePermissionInfo
+import com.tencent.devops.project.pojo.ProjectCreateInfo
 import com.tencent.devops.project.pojo.ProjectCreateUserDTO
 import com.tencent.devops.project.pojo.ProjectVO
 import com.tencent.devops.project.pojo.Result
