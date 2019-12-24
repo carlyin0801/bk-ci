caad7fc84cf7cbfb75efb8adec0fe1cb7efa692c
cao150260 <fitzcao@tencent.com>
2019-12-24 23:24:58 +0800

    --story=856857567 项目不一致问题

--- src/backend/ci/ext/tencent/project/api-project-op/src/main/kotlin/com/tencent/devops/project/api/op/OPProjectResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-op/src/main/kotlin/com/tencent/devops/project/api/op/OPProjectResource.kt
@@ -43,6 +43,7 @@ import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.PUT
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.Context
@@ -163,6 +164,7 @@ interface OPProjectResource {
     @Path("/{projectId}/syn")
     fun synProject(
         @ApiParam(value = "项目code", required = true)
+        @PathParam("projectId")
         projectCode: String
     ): Result<Boolean>
 
