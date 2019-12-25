5128f6e4e6a6580d13b020c5877e9f570a073bf2
cao150260 <fitzcao@tencent.com>
2019-12-25 21:31:36 +0800

    --story=856857567 项目不一致问题

--- src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/impl/OpProjectServiceImpl.kt
+++ src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/impl/OpProjectServiceImpl.kt
@@ -219,7 +219,7 @@ class OpProjectServiceImpl @Autowired constructor(
 //                )
 //            )
             }
-            logger.info("project syn success, projectCode[$projectCode]")
+            logger.info("project syn success, projectCode[$projectCode], creator[${projectInfo.creator}]")
             isSyn = true
         }
         if (!isSyn) {
