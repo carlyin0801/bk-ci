002553c63f24ecdf31003d41a31c5c4e3b379830
warmli <957569558@qq.com>
2020-01-10 09:55:19 +0800

    --story=856374251 【流水线插件】go版功能完善 - 支持获取构建参数

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/dao/PipelineBuildVarDao.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/dao/PipelineBuildVarDao.kt
@@ -90,12 +90,13 @@ class PipelineBuildVarDao @Autowired constructor() {
     fun getVars(dslContext: DSLContext, buildId: String?, projectId: String?, pipelineId: String?, key: String?): MutableMap<String, String> {
         with(T_PIPELINE_BUILD_VAR) {
             val where = dslContext.selectFrom(this)
+                .where()
             if (buildId != null)
                 where.and(BUILD_ID.eq(buildId))
             if (projectId != null)
                 where.and(PROJECT_ID.eq(projectId))
             if (pipelineId != null)
-                where.and(PIPELINE_ID.qe(pipelineId))
+                where.and(PIPELINE_ID.eq(pipelineId))
             if (key != null)
                 where.and(KEY.eq(key))
 
