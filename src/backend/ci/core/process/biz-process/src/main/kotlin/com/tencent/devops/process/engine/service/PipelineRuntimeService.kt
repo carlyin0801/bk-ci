0a0be4ae45ecedf0cbe636111ac1cd4e1f5c44bb
Royalhuang <royalhuang@tencent.com>
2020-01-10 20:45:11 +0800

    bugfix: 构建号解析错误导致无法显示 #537

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeService.kt
@@ -1957,6 +1957,9 @@ class PipelineRuntimeService @Autowired constructor(
         if (allVariable[PIPELINE_RETRY_COUNT] == null) return
 
         val triggerContainer = model.stages[0].containers[0] as TriggerContainer
+        val params = allVariable.filter {
+            it.key.startsWith(SkipElementUtils.prefix) || it.key == BUILD_NO || it.key == PIPELINE_RETRY_COUNT
+        }.toMutableMap()
 
         if (triggerContainer.buildNo != null) {
             val buildNo = getBuildNo(pipelineId)
@@ -1964,18 +1967,14 @@ class PipelineRuntimeService @Autowired constructor(
                 projectId = projectId, pipelineId = pipelineId,
                 buildId = buildId, varName = BUILD_NO, varValue = buildNo
             )
+            params[BUILD_NO] = buildNo.toString()
         }
-        // 只有在构建参数中的才设置
-        val params = allVariable.filter {
-            it.key.startsWith(SkipElementUtils.prefix) || it.key == BUILD_NO || it.key == PIPELINE_RETRY_COUNT
-        }
+
         if (triggerContainer.params.isNotEmpty())
             params.plus(triggerContainer.params.map {
-                if (allVariable.containsKey(it.id)) { // 做下真实传值的替换
-                    it.id to allVariable[it.id]
-                } else {
-                    it.id to it.defaultValue
-                }
+                // 真实传值的替换
+                if (allVariable.containsKey(it.id)) it.id to allVariable[it.id]
+                else it.id to it.defaultValue
             }.toMap())
         buildStartupParamService.addParam(
             projectId = projectId,
