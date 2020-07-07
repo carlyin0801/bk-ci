20f28dcd1c7d7cf8c7e961601868e841383ce818
carlyin-tx <449036249@qq.com>
2020-07-07 14:53:27 +0800

    perf: 研发商店组件修改调试项目接口优化 #1975

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/common/impl/StoreMemberServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/common/impl/StoreMemberServiceImpl.kt
@@ -276,7 +276,7 @@ abstract class StoreMemberServiceImpl : StoreMemberService {
             // 更新用户的调试项目
             storeProjectRelDao.updateUserStoreTestProject(
                 dslContext = context,
-                userId = userId,
+                userId = storeMember,
                 projectCode = projectCode,
                 storeProjectType = StoreProjectTypeEnum.TEST,
                 storeCode = storeCode,
