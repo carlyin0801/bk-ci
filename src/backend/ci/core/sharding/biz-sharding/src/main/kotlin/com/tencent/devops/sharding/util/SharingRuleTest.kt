cd8d59082c1dab22a948a7e8900a0a707bed7a1a
carlyin <449036249@qq.com>
2022-01-18 21:02:17 +0800

    test

--- src/backend/ci/core/sharding/biz-sharding/src/main/kotlin/com/tencent/devops/sharding/util/SharingRuleTest.kt
+++ src/backend/ci/core/sharding/biz-sharding/src/main/kotlin/com/tencent/devops/sharding/util/SharingRuleTest.kt
@@ -7,13 +7,13 @@ import java.io.File
 import java.util.*
 
 fun main() {
-    val file = File("C:\\Users\\carlyin\\Desktop\\test\\pcg_project_20220118.txt")
+    val file = File("C:\\Users\\carlyin\\Desktop\\test\\sub-db-project\\process02_project_20220119.txt")
     val lines = file.readLines()
     lines.forEach { line ->
         val headers = mapOf(
             "Cookie" to "bk_ticket=XKw17787-U5agVEem2sw6X8Fv-2aoso_uTfuXzj3wDU;",
             "Content-Type" to "application/json",
-            "X-DEVOPS-UID" to "carlyin",
+            "X-DEVOPS-UID" to "test",
             "X-DEVOPS-TOKEN" to "vC9DOvUtqo4X3DORKis1RRX1XNGu8YFw",
             "X-DEVOPS-PROJECT-ID" to "grayproject"
         )
@@ -21,10 +21,16 @@ fun main() {
             "routingName" to line,
             "routingRule" to "ds_2"
         )
-        val response = OkhttpUtils.doPost("http://devops.oa.com/ms/project/api/op/sharding/routing/rules/add", JsonUtil.toJson(paramMap), headers)
-        val data = response.body()?.string()
-        val jsonObject = if (data != null) JsonUtil.toMap(data) else null
-        val result = jsonObject?.get("data")?.toString()
-        println("${DateTimeUtil.formatDate(Date())} routingName:$line,result:$result")
+        try {
+            val response = OkhttpUtils.doPost("http://dev.devops.oa.com/ms/project/api/op/sharding/routing/rules/add", JsonUtil.toJson(paramMap), headers)
+            val data = response.body()?.string()
+            val jsonObject = if (data != null) JsonUtil.toMap(data) else null
+            val status = jsonObject?.get("status")?.toString()
+            val message = jsonObject?.get("message")?.toString()
+            println("${DateTimeUtil.formatDate(Date())} routingName:$line,status:$status,message:$message")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("${DateTimeUtil.formatDate(Date())} routingName:$line add fail")
+        }
     }
 }
