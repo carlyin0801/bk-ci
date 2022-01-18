11dfa4c1747fd4ff17b6f376fee31d26f756a579
carlyin <449036249@qq.com>
2022-01-18 10:41:42 +0800

    test

---
+++ src/backend/ci/core/sharding/biz-sharding/src/main/kotlin/com/tencent/devops/sharding/util/SharingRuleQueryTest.kt
@@ -0,0 +1,22 @@
+package com.tencent.devops.sharding.util
+
+import com.tencent.devops.common.api.util.DateTimeUtil
+import com.tencent.devops.common.api.util.JsonUtil
+import com.tencent.devops.common.api.util.OkhttpUtils
+import java.io.File
+import java.util.*
+
+fun main() {
+    val headers = mapOf(
+        "Cookie" to "bk_ticket=XKw17787-U5agVEem2sw6X8Fv-2aoso_uTfuXzj3wDU;",
+        "Content-Type" to "application/json",
+        "X-DEVOPS-UID" to "carlyin",
+        "X-DEVOPS-TOKEN" to "vC9DOvUtqo4X3DORKis1RRX1XNGu8YFw",
+        "X-DEVOPS-PROJECT-ID" to "grayproject"
+    )
+    val response = OkhttpUtils.doGet("http://devops.oa.com/ms/project/api/op/sharding/routing/rules/names/git_300586/get", headers)
+    val data = response.body()?.string()
+    val jsonObject = if (data != null) JsonUtil.toMap(data) else null
+    val result = jsonObject?.get("data")?.toString()
+    println("${DateTimeUtil.formatDate(Date())} result:$result")
+}
--- src/backend/ci/core/sharding/biz-sharding/src/main/kotlin/com/tencent/devops/sharding/util/SharingRuleTest.kt
+++ src/backend/ci/core/sharding/biz-sharding/src/main/kotlin/com/tencent/devops/sharding/util/SharingRuleTest.kt
@@ -5,15 +5,13 @@ import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.common.api.util.OkhttpUtils
 import java.io.File
 import java.util.*
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
 
 fun main() {
-    val file = File("C:\\Users\\carlyin\\Desktop\\test\\gitci_project.txt")
+    val file = File("C:\\Users\\carlyin\\Desktop\\test\\pcg_project_20220118.txt")
     val lines = file.readLines()
     lines.forEach { line ->
         val headers = mapOf(
-            "Cookie" to "bk_ticket=A_PRwxqbZ79j2tVpqWCTo9F0SfCVNHJ_FFSF7aVjeQE;",
+            "Cookie" to "bk_ticket=XKw17787-U5agVEem2sw6X8Fv-2aoso_uTfuXzj3wDU;",
             "Content-Type" to "application/json",
             "X-DEVOPS-UID" to "carlyin",
             "X-DEVOPS-TOKEN" to "vC9DOvUtqo4X3DORKis1RRX1XNGu8YFw",
@@ -21,7 +19,7 @@ fun main() {
         )
         val paramMap = mapOf(
             "routingName" to line,
-            "routingRule" to "ds_0"
+            "routingRule" to "ds_2"
         )
         val response = OkhttpUtils.doPost("http://devops.oa.com/ms/project/api/op/sharding/routing/rules/add", JsonUtil.toJson(paramMap), headers)
         val data = response.body()?.string()
