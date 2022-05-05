fce7364757475f88d113be6f9e9fd441b8cc5675
yjieliang <yundantengteng@163.com>
2022-05-05 18:27:22 +0800

    issue_6587_metrics_dev  2022/5/5

--- src/backend/ci/core/metrics/api-metrics/src/main/kotlin/com/tencent/metrics/api/UserPipelineFailResource.kt
+++ src/backend/ci/core/metrics/api-metrics/src/main/kotlin/com/tencent/metrics/api/UserPipelineFailResource.kt
@@ -6,6 +6,7 @@ import com.tencent.devops.common.api.pojo.Result
 import com.tencent.metrics.pojo.`do`.PipelineFailDetailInfoDO
 import com.tencent.metrics.pojo.vo.PipelineFailInfoQueryReqVO
 import com.tencent.devops.common.api.pojo.Page
+import com.tencent.devops.common.web.annotation.BkField
 import com.tencent.metrics.pojo.vo.PipelineFailSumInfoVO
 import com.tencent.metrics.pojo.vo.PipelineFailTrendInfoVO
 import io.swagger.annotations.Api
@@ -66,9 +67,11 @@ interface UserPipelineFailResource {
         @ApiParam("查询条件", required = true)
         queryReqVo: PipelineFailInfoQueryReqVO,
         @ApiParam("页码", required = true, defaultValue = "1")
+        @BkField(minLength = 1)
         @QueryParam("page")
         page: Int,
         @ApiParam("每页大小", required = true, defaultValue = "10")
+        @BkField(minLength = 10, maxLength = 100)
         @QueryParam("pageSize")
         pageSize: Int
     ): Result<Page<PipelineFailDetailInfoDO>>
